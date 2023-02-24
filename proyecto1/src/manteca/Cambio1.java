bca2e01b64707a5629175ae1157927fd2c7542ad
alvrodmol <alvarorodriguezmolina0@gmail.com>
2023-02-24 18:03:16 +0100

    Agregado todo al repositorio

--- proyecto1/src/manteca/Cambio1.java
+++ proyecto1/src/manteca/Cambio1.java
@@ -4,6 +4,6 @@ public class Cambio1{
 
 	public static void main(String[] args) {
 
-		System.out.println("Hola mundo java-git");
+		System.out.println("Pruebas git. clase cambio1");
 }}
 
--- proyecto1/src/prueba1/Principal.java
+++ proyecto1/src/prueba1/Principal.java
@@ -5,6 +5,10 @@ public class Principal {
 	public static void main(String[] args) {
 
 		System.out.println("Hola mundo java-git");
+		System.out.println("Hola mundo java-git");
+		System.out.println("Hola mundo java-git");
+		System.out.println("Hola mundo java-git");
+
 	}
 
 }
